9753e7159ad4b504613e5ff195d5b9190c57fe38
kiran patel <kiran.spaceo@gmail.com>
2014-06-16 18:38:18 +0530

    KFacebook
    Facebook all methods are integrated
    login,logout,Fetch Friends list,Post Text and Image on FBWall
    Post Text and image to FB’ Friends’s wall
    Twitter Login,Fetch Twitter’s List

---
+++ kFacebookTests/kFacebookTests.m
@@ -0,0 +1,34 @@
+//
+//  kFacebookTests.m
+//  kFacebookTests
+//
+//  Created by macmini17 on 13/05/14.
+//  Copyright (c) 2014 macmini17. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kFacebookTests : XCTestCase
+
+@end
+
+@implementation kFacebookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
