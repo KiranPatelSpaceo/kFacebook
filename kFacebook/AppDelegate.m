daf2ab4a81d60c9b358944571c84ce2884746111
kiran patel <kiran.spaceo@gmail.com>
2014-06-17 11:39:59 +0530

    Commit 1.1
    Commit 1.1

--- kFacebook/AppDelegate.m
+++ kFacebook/AppDelegate.m
@@ -12,6 +12,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [application setStatusBarStyle:UIStatusBarStyleLightContent];
+
     // Override point for customization after application launch.
     return YES;
 }
